4cba72190bbde15a4bc4c0b3daa6fef344f9b94f
HAMEL Julien <julienhamel.h@gmail.com>
2023-06-01 00:54:55 +0200

    feat: add sign form style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head"
 import SignUp from "./sign-up"
 
 export default function Home() {
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,10 +1,46 @@
-import { Input } from "@nextui-org/react"
+import { Button, Card, Container, Input, Spacer, Text } from "@nextui-org/react"
 
 const SignUp = () => {
   return (
-    <div>
-      <Input clearable bordered labelPlaceholder="Name" initialValue="NextUI" />
-    </div>
+    <Container
+      display="flex"
+      alignItems="center"
+      justify="center"
+      css={{ minHeight: "100vh" }}
+    >
+      <Card css={{ mw: "420px", p: "20px" }}>
+        <Text
+          size={26}
+          weight="bold"
+          css={{
+            as: "center",
+            mb: "20px",
+            color: "$primary",
+          }}
+        >
+          Inscription
+        </Text>
+        <Input
+          clearable
+          bordered
+          fullWidth
+          color="primary"
+          size="lg"
+          placeholder="Email"
+        />
+        <Spacer y={1} />
+        <Input.Password
+          clearable
+          bordered
+          fullWidth
+          color="primary"
+          size="lg"
+          placeholder="Password"
+        />
+        <Spacer y={1} />
+        <Button>S'inscire</Button>
+      </Card>
+    </Container>
   )
 }
 
