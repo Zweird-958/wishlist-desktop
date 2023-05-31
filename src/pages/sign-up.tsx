ffff0f502287c5af5d8419357bbc033ec7c0bfbd
HAMEL Julien <julienhamel.h@gmail.com>
2023-06-01 00:24:19 +0200

    initial Commit

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,11 @@
+import { Input } from "@nextui-org/react"
+
+const SignUp = () => {
+  return (
+    <div>
+      <Input clearable bordered labelPlaceholder="Name" initialValue="NextUI" />
+    </div>
+  )
+}
+
+export default SignUp
