b24f4223b9ed5a6aa3bd93cb35b92582b2b3ab9c
HAMEL Julien <julienhamel.h@gmail.com>
2023-06-01 15:25:30 +0200

    chore: upgrade to next ui v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+import { nextui } from "@nextui-org/react"
+
+// /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,9 +9,11 @@ module.exports = {
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
