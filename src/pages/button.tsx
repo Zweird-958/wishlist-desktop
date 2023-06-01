3ab3ae45b74a566f5c126b91541389a9c0cd37f5
HAMEL Julien <julienhamel.h@gmail.com>
2023-06-01 17:07:41 +0200

    refactor: delete useless page

--- src/pages/button.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@nextui-org/react"
-
-export default function Page() {
-  return (
-    <div className="w-screen ">
-      <Button color="primary">Button</Button>
-    </div>
-  )
-}
