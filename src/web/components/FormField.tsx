daaea877ea864bfdbbcc2d6a5848a87374666518
HAMEL Julien <julienhamel.h@gmail.com>
2023-06-04 12:45:12 +0200

    refactor: use error message attribut

--- src/web/components/FormField.tsx
+++ src/web/components/FormField.tsx
@@ -12,22 +12,18 @@ const FormField = (props: Props) => {
   const [field, meta] = useField(otherProps)
 
   return (
-    <>
-      <Input
-        label={label}
-        color="primary"
-        variant="bordered"
-        size="sm"
-        radius="lg"
-        fullWidth
-        labelPlacement="inside"
-        {...field}
-        {...otherProps}
-      />
-      {meta.touched && meta.error ? (
-        <p className="px-2 text-sm">{meta.error}</p>
-      ) : null}
-    </>
+    <Input
+      label={label}
+      color="primary"
+      variant="bordered"
+      size="sm"
+      radius="lg"
+      fullWidth
+      labelPlacement="inside"
+      {...field}
+      {...otherProps}
+      errorMessage={meta.error}
+    />
   )
 }
 
