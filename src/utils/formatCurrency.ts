424a25ef33836856427a914a2164be740701ec18
HAMEL Julien <julienhamel.h@gmail.com>
2023-06-12 10:03:25 +0200

    feat: default dollar format

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,15 +1,15 @@
+const currencySymbols: { [key: string]: string } = {
+  DOLLAR: "USD",
+  EURO: "EUR",
+  POUND: "GBP",
+}
+
 const formatCurrency = (price: number, currency: string) => {
   const locale = window.navigator.language
 
-  const currencySymbols: { [key: string]: string } = {
-    DOLLAR: "USD",
-    EURO: "EUR",
-    POUND: "GBP",
-  }
-
   return new Intl.NumberFormat(locale, {
     style: "currency",
-    currency: currencySymbols[currency],
+    currency: currencySymbols[currency] || currencySymbols.DOLLAR,
   }).format(price)
 }
 
