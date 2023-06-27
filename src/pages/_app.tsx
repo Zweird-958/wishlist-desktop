193f8e656c8a8bc3fdcf458e052517ea0d05f1be
HAMEL Julien <julienhamel.h@gmail.com>
2023-06-27 13:15:36 +0200

    refactor: type props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/globals.css"
+import { AppProps } from "next/app"
 import AppBar from "@/web/components/AppBar"
 import { NextUIProvider } from "@nextui-org/react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const queryClient = new QueryClient()
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
       <NextUIProvider>
