6ab3bed8b84d579c6b704bb2693f852f041d24dd
HAMEL Julien <julienhamel.h@gmail.com>
2023-09-05 22:08:19 +0200

    feat: add loader to load locale

--- src/pages/[[...paths]].tsx
+++ src/pages/[[...paths]].tsx
@@ -1,8 +1,13 @@
 export { getStaticProps, getStaticPaths } from "i18next-ssg/Redirect"
+import Loading from "@/web/components/Loading"
 import { useRootPathRedirect } from "i18next-ssg"
 
 export default function Page() {
   useRootPathRedirect()
 
-  return <div>Redirecting...</div>
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Loading />
+    </div>
+  )
 }
